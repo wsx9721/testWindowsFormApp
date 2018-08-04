c309988f905448341826f331dabbaa8cc7c8a701
张昊岚 <zhl19961214@vip.qq.com>
2018-08-04 16:11:43 +0800

    init

--- testWindowsFormsApp/Program.cs
+++ testWindowsFormsApp/Program.cs
@@ -1,22 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+using System.Text;
+using System.Diagnostics;
+using System.Threading;
 
-namespace testWindowsFormsApp
-{
-    static class Program
+namespace CSharpPerformance
+{//该程序可以实时监控所有进程或者指定进程的工作集、私有工作集
+    class Program
     {
-        /// <summary>
-        /// 应用程序的主入口点。
-        /// </summary>
-        [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //新建一个Stopwatch变量用来统计程序运行时间
+            Stopwatch watch = Stopwatch.StartNew();
+            //获取本机运行的所有进程ID和进程名,并输出哥进程所使用的工作集和私有工作集
+            foreach (Process ps in Process.GetProcesses())
+            {
+                PerformanceCounter pf1 = new PerformanceCounter("Process", "Working Set - Private", ps.ProcessName);
+                PerformanceCounter pf2 = new PerformanceCounter("Process", "Working Set", ps.ProcessName);
+                Console.WriteLine("{0}:{1}  {2:N}KB", ps.ProcessName, "工作集(进程类)", ps.WorkingSet64 / 1024);
+                Console.WriteLine("{0}:{1}  {2:N}KB", ps.ProcessName, "工作集        ", pf2.NextValue() / 1024);
+                //私有工作集
+                Console.WriteLine("{0}:{1}  {2:N}KB", ps.ProcessName, "私有工作集    ", pf1.NextValue() / 1024);
+
+            }
+
+            watch.Stop();
+            Console.WriteLine(watch.Elapsed);
+            Console.ReadLine();
         }
     }
 }
