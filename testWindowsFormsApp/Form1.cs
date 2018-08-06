d8e1dcd63231cea45369ec4c052485eeb5e9f327
张昊岚 <zhl19961214@vip.qq.com>
2018-08-06 20:34:13 +0800

    day_one

--- testWindowsFormsApp/Form1.cs
+++ testWindowsFormsApp/Form1.cs
@@ -16,5 +16,20 @@ namespace testWindowsFormsApp
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = ("ok");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = ("cancel");
+        }
     }
 }
